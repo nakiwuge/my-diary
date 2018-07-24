72f01ba4001bda8a33fff268265b02eb18f4c276
Miriam <nakiwuge@gmail.com>
2018-07-24 09:40:50 +0300

    remove UI from api

--- UI/js/main.js
+++
@@ -1,5 +0,0 @@
-var today=  new Date();
-var year= today.getFullYear();
-var month= today.getMonth() + 1;
-var day=today.getDate();
-document.getElementById("date").innerHTML = month + "/" + day + "/" + year;
