90e8e9a404a3935ea986cb6d2491e1c2689d00b8
Miriam <nakiwuge@gmail.com>
2018-07-25 07:03:20 +0300

    initial commit

--- v1/test_endpoints.py
+++ v1/test_endpoints.py
@@ -5,20 +5,25 @@ import json
 class FlaskTestCase(unittest.TestCase):
     def setUp(self):
         self.tester = app.test_client(self)
-        self.data=  {"id":1,"content": "i learnt thow to make templates","date": "25-july-2018","id": 7,"title": "templathiahijing"}
+        self.data=  {"id":"1","content":"i learnt thow to make templates","date":"25-july-2018","title": "templating"}
      
-       
+    #test get all erntries end point  
     def test_get_all_enteries(self):
         response = self.tester.get('/api/v1/entries', data=self.data)
         self.assertEqual(response.status_code, 200)
-    def test_get_results_from_all_enteries(self):
+
+     
+    #test post entry end point 
+    def test_post_entery(self):
         response = self.tester.post('/api/v1/entries',data=self.data)
         self.assertEqual(response.status_code, 200)
+
+
+    #test get entry_id end point
+    def test_get_entry_id(self):
+        response = self.tester.get('/api/v1/entries/1',data=self.data)
+        self.assertEqual(response.status_code, 200)
  
-    
-    
-
-
        
         
 
