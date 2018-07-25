ca4fb2cde4f2704f251ba6f29ae53bc68124d96d
Miriam <nakiwuge@gmail.com>
2018-07-25 03:46:31 +0300

    modify views.py

--- v1/views.py
+++ v1/views.py
@@ -12,20 +12,25 @@ def home():
 def entries():
     #getting an entry
     if request.method == 'GET':
-        return jsonify({'entries':all_entries}) 
+        #check if all_entries is empty
+        if not all_entries:
+            return "no entries added , please add an entry"
+        return jsonify(all_entries) 
               
     else:
+        #getting an entry
         add_entry = request.get_json() 
         all_entries.append(add_entry)
-        return jsonify({'entries':all_entries})
+        return jsonify(all_entries)
        
-@app.route('/api/v1/entries/<int:id>' , methods=['GET','PUT'])
-def content(id):
+@app.route('/api/v1/entries/<string:entry_id>' , methods=['GET','PUT'])
+def content(entry_id):
     if request.method == 'GET':
-        get_content=[x for x in all_entries  if x['id'] == id] 
-        return jsonify({id:get_content[0]})
+        #get_content=[x for x in all_entries  if x.values() == entry_id] 
+        get_content=[x for x in all_entries for y,z in x.items()if z == entry_id]
+        return jsonify(get_content[0])
 
     else:
-        get_content=[x for x in all_entries  if x['id'] == id] 
+        get_content=[x for x in all_entries for y,z in x.items()if z == entry_id] 
         get_content[0]=request.get_json()
-        return jsonify({id:get_content[0]})
+        return jsonify(get_content[0])
