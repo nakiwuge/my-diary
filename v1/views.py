3ff031f69102a104e0dca261aaeabd358e75f01f
Miriam <nakiwuge@gmail.com>
2018-07-25 14:17:40 +0300

    modify views.py

--- v1/views.py
+++ v1/views.py
@@ -15,8 +15,8 @@ def entries():
     if request.method == 'GET':
         #check if all_entries is empty
         if not all_entries:
-            return "no entries added , please add an entry"
-        return jsonify(all_entries) 
+            return "no entries added, please add an entry",404
+        return jsonify(all_entries),200
               
     else:
         #getting an entry
@@ -24,22 +24,23 @@ def entries():
         for key in add_entry:
             #check if user is not using the required keys
             if key not in ["id","content","date","title"]:
-                return "key should either be id ,content ,date and title"
+                return "key should either be id ,content ,date and title",403
             
         all_entries.append(add_entry)
-        return jsonify(all_entries)
+        return jsonify(all_entries),200
       
 @app.route('/api/v1/entries/<string:entry_id>' , methods=['GET','PUT'])
 def content(entry_id):
     #get entry by id
     if request.method == 'GET':
+       
         #check if all_entries is empty
         if not all_entries:
-            return "no entries added , please add an entry"
+            return "no entries added , please add an entry",404
         #check if id entered is in the data base
         for entry in all_entries:
                 if entry_id!=entry["id"]:
-                    return "the is no entry with that the provided id"
+                    return "the is no entry with that the provided id",404
     
         get_content =[entry for entry in all_entries if entry['id']==entry_id]
         return jsonify(get_content[0])
@@ -47,8 +48,9 @@ def content(entry_id):
     else:
         #modify entry by id
         get_content =[entry for entry in all_entries if entry['id']==entry_id]
+        get_content =[entry for entry in all_entries if entry['id']==entry_id]
         get_content[0]['title']=request.json['title']
         get_content[0]['content']=request.json['content']
         get_content[0]['date']=request.json['date']
 
-        return jsonify(get_content[0])
+        return jsonify(get_content[0]),200
