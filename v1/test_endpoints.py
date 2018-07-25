588f5230ceb32d94c17125eeced0b67131e25f9c
Miriam <nakiwuge@gmail.com>
2018-07-25 03:45:30 +0300

    add test for post endpoint

--- v1/test_endpoints.py
+++ v1/test_endpoints.py
@@ -5,12 +5,18 @@ import json
 class FlaskTestCase(unittest.TestCase):
     def setUp(self):
         self.tester = app.test_client(self)
-        self.data=  {"content": "i learnt thow to make templates","date": "25-july-2018","id": 7,"title": "templathiahijing"}
+        self.data=  {"id":1,"content": "i learnt thow to make templates","date": "25-july-2018","id": 7,"title": "templathiahijing"}
      
        
-    def test_get_results_from_all_enteries(self):
-        response = self.tester.get('/api/v1/entries')
+    def test_get_all_enteries(self):
+        response = self.tester.get('/api/v1/entries', data=self.data)
         self.assertEqual(response.status_code, 200)
+    def test_get_results_from_all_enteries(self):
+        response = self.tester.post('/api/v1/entries',data=self.data)
+        self.assertEqual(response.status_code, 200)
+ 
+    
+    
 
 
        
