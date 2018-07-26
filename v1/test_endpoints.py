c2e3b95603324aa82966a627c4d1a79769541959
Miriam <nakiwuge@gmail.com>
2018-07-27 00:08:41 +0300

    add proper docummentation

--- v1/test_endpoints.py
+++ v1/test_endpoints.py
@@ -2,82 +2,88 @@ from v1 import app
 import unittest
 import json
 
+
 class EntryTestCase(unittest.TestCase):
     def setUp(self):
         self.tester = app.test_client(self) 
-        self.data=json.dumps({'id':'3','title':'this', 'content':'this content','date':'5-6-8'})
-        
-    #testing if database is empty
+        self.data = json.dumps({
+            'id': '3', 
+            'title': 'this',
+            'content': 'this content', 
+            'date': '5-6-8'})   
+    '''testing if database is empty'''
     def test_for_error_returned_when_no_entries(self):
         response = self.tester.get('/api/v1/entries')
         self.assertEqual(response.status_code, 404)
         self.assertIn(b"no entries added, please add an entry", response.data)
 
-     #testing the get with the data
+    '''testing the get with the data'''
     def test_success_for_get_all_entries(self):
         self.tester.post(
-            '/api/v1/entries',
-            data=self.data,
-            content_type='application/json'
-            )
+        '/api/v1/entries',
+        data=self.data,
+        content_type='application/json'
+        )
         response = self.tester.get('/api/v1/entries')
         self.assertEqual(response.status_code, 200)
 
-    #testing for invalid keys passed
+    '''testing for invalid keys passed'''
     def test_for_error_returned_when_invalid_keys(self):
-        dummy_data=json.dumps({
-            'id':'3',
-            'tit':'this', 
-            'body':'this content',
-            'date':'5-6-8'
+        dummy_data = json.dumps({
+            'id': '3',
+            'tit': 'this', 
+            'body': 'this content',
+            'date': '5-6-8'
             })
-        response= self.tester.post(
+        response = self.tester.post(
             '/api/v1/entries', 
             data=dummy_data,
             content_type='application/json'
             )
         self.assertEqual(response.status_code, 403)
-        self.assertIn(b"key should either be id ,content ,date and title", response.data)
-    
-    #testing if the value entered is empty
+        self.assertIn(
+            b"key should either be id ,content ,date and title", 
+            response.data
+            )    
+    '''testing if the value entered is empty'''
     def test_for_error_returned_when_empty_values(self):
-        test_data=json.dumps({
-            'id':'3',
-            'title':'', 
-            'content':'',
-            'date':'5-6-8'
+        test_data = json.dumps({
+            'id': '3',
+            'title': '', 
+            'content': '',
+            'date': '5-6-8'
             })
-        response= self.tester.post(
+        response = self.tester.post(
             '/api/v1/entries', 
             data=test_data,
             content_type='application/json'
             )
         self.assertEqual(response.status_code, 403)
-        self.assertIn(b"one or more of your key values are empty. please check and try again", response.data)
+        self.assertIn(
+            b"one or more of your key values are empty. please check and try again", response.data)
 
-    #testing if specified entry is not in the database
+    '''testing if specified entry is not in the database'''
     def test_for_error_for_no_specified_entry(self):
         response = self.tester.get('/api/v1/entries/id')
         self.assertEqual(response.status_code, 404)
         self.assertIn(b"no entries added, please add an entry", response.data)
 
-    #testing wether a specific is succesfully fetched
-    def test_for_sucessfull_get_specific_entry(self ):
+    '''#testing wether a specific is succesfully fetched'''
+    def test_for_sucessfull_get_specific_entry(self):
         self.tester.post(
             '/api/v1/entries',
             data=self.data,
             content_type='application/json'
             )
         response = self.tester.get('/api/v1/entries/3')
-        self.assertEqual(response.status_code, 200)
-        
-    #taste if pu method is succesful
-    def test_for_success__on_update(self ):
-        new_data=json.dumps({
-            'id':'3',
-            'title':'new title', 
-            'content':'content stuff',
-            'date':'5-6-8'
+        self.assertEqual(response.status_code, 200)       
+    '''taste if pu method is succesful'''
+    def test_for_success__on_update(self):
+        new_data = json.dumps({
+            'id': '3',
+            'title': 'new title', 
+            'content': 'content stuff',
+            'date': '5-6-8'
             })
         self.tester.post(
             '/api/v1/entries',
@@ -90,8 +96,6 @@ class EntryTestCase(unittest.TestCase):
             content_type='application/json'
             )
         response = self.tester.get('/api/v1/entries/3')
-        self.assertEqual(response.status_code, 200) 
-    
-
-if __name__ =="__main__":
+        self.assertEqual(response.status_code, 200)    
+if __name__ == "__main__":
     unittest.main()
--- v1/views.py
+++ v1/views.py
@@ -1,47 +1,49 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
 from v1 import app
 import json
 
-all_entries = []
-  
-@app.route('/api/v1/entries' , methods=['GET','POST'])
+all_entries = [] 
+
+
+@app.route('/api/v1/entries', methods=['GET', 'POST'])
 def entries():
-    #getting an entry
+    '''getting an entry'''
     if request.method == 'GET':
-        #check if all_entries is empty
-        if len(all_entries)==0:
-            return "no entries added, please add an entry",404
-        return jsonify(all_entries),200           
+        '''check if all_entries is empty'''
+        if len(all_entries) == 0:
+            return "no entries added, please add an entry", 404
+        return jsonify(all_entries), 200           
     else:
-        #post an entry
+        '''post an entry'''
         add_entry = request.get_json()
         for key in add_entry:
-            #check if user is not using the required keys
-            keys=["id","content","date","title"]
-            if key not in  keys :
-                return "key should either be id ,content ,date and title",403 
-        #check for empty values
+            '''check if user is not using the required keys'''
+            keys = ["id", "content", "date", "title"]
+            if key not in keys:
+                return "key should either be id ,content ,date and title", 403 
+        '''check for empty values'''
         for key in add_entry:
-            if add_entry[key]=="":
-                 return "one or more of your key values are empty. please check and try again",403
+            if add_entry[key] == "":
+                return "one or more of your key values are empty.please check and try again", 403
 
         all_entries.append(add_entry)
-        return jsonify(all_entries),200
-      
-@app.route('/api/v1/entries/<string:entry_id>' , methods=['GET','PUT'])
+        return jsonify(all_entries), 200
+
+
+@app.route('/api/v1/entries/<string:entry_id>', methods=['GET', 'PUT'])
 def content(entry_id):
-    #get entry by id
+    '''get entry by id'''
     if request.method == 'GET':
-        #check if all_entries is empty
-        if len(all_entries)==0:
-            return "no entries added, please add an entry",404
-        get_content =[entry for entry in all_entries if entry['id']==entry_id]
+        '''check if all_entries is empty'''
+        if len(all_entries) == 0:
+            return "no entries added, please add an entry", 404
+        get_content = [entry for entry in all_entries if entry['id'] == entry_id]
         return jsonify(get_content[0])
     else:
-        #modify entry by id
-        get_content =[entry for entry in all_entries if entry['id']==entry_id]
-        get_content =[entry for entry in all_entries if entry['id']==entry_id]
-        get_content[0]['title']=request.json['title']
-        get_content[0]['content']=request.json['content']
-        get_content[0]['date']=request.json['date']
-        return jsonify(get_content[0]),200
+        '''#modify entry by id'''
+        get_content = [entry for entry in all_entries if entry['id'] == entry_id]
+        get_content = [entry for entry in all_entries if entry['id'] == entry_id]
+        get_content[0]['title'] = request.json['title']
+        get_content[0]['content'] = request.json['content']
+        get_content[0]['date'] = request.json['date']
+        return jsonify(get_content[0]), 200
